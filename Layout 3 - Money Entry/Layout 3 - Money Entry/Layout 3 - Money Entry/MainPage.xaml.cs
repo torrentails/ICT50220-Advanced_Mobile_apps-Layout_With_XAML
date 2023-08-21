530e5d6d9d07045375aec04635d64325c4ac916d
Alex Sullivan <torrentails@torrentails.com>
2023-08-21 17:38:25 +0800

    Layout 1 through 4

---
+++ Layout 3 - Money Entry/Layout 3 - Money Entry/Layout 3 - Money Entry/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Layout_3___Money_Entry
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
